d79293babda745b5ffc5345deeb465e8e96cf366
Rei <snowy.sunny.dazing.us@gmail.com>
2024-07-10 02:31:17 +0900

    Update

--- src/Television_Tray.py
+++ src/Television_Tray.py
@@ -1,15 +1,15 @@
 import os
 import sys
+import threading
+import time
+
+from pypresence import Presence
 
 import pystray
 from PIL import Image
 
 
-class TelevisionMenu(pystray.Menu):
-    item_exit = pystray.MenuItem("Exit", action_exit)
-
-    def __init__(self):
-        super().__init__(self.item_exit)
+TRAY_CLOSED = False
 
 
 def resource_path(relative_path: str):
@@ -22,10 +22,64 @@ def resource_path(relative_path: str):
 
 
 def action_exit(icon: pystray.Icon, _):
+    global TRAY_CLOSED
+    TRAY_CLOSED = True
     icon.stop()
 
 
-image_path = resource_path("television.ico")
-image = Image.open(image_path)
-icon = pystray.Icon(name="Television", icon=image, title="Television", menu=TelevisionMenu())
-icon.run()
+# Pystray
+class TelevisionMenu(pystray.Menu):
+    item_exit = pystray.MenuItem("Exit", action_exit)
+
+    def __init__(self):
+        super().__init__(self.item_exit)
+
+
+IMAGE_PATH = resource_path("television.ico")
+IMAGE = Image.open(IMAGE_PATH)
+ICON = pystray.Icon(name="Television", icon=IMAGE, title="Television", menu=TelevisionMenu())
+
+
+# PyPresence
+START = time.time()
+
+APPLICATION_ID = "1260226649870565537"
+RPC = Presence(APPLICATION_ID)
+NAME = os.path.basename(__file__).split(".")[0]
+
+
+def run_rpc():
+    RPC.connect()
+    print(f"Presence {NAME} has started.")
+
+    try:
+        while not TRAY_CLOSED:
+            RPC.update(
+                details="Watching Television",
+                state="Switching Channels",
+                large_image="television",
+                large_text="Watching Television",
+                start=int(START),
+            )
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print(f"Presence {NAME} has stopped due to a keyboard interrupt.")
+    except Exception as e:
+        print(f"Presence {NAME} has stopped due to an error: {e}")
+
+    RPC.close()
+    print(f"Presence {NAME} has stopped.")
+
+
+def run_icon():
+    ICON.run()
+
+
+thread_icon = threading.Thread(target=run_icon, daemon=True)
+thread_rpc = threading.Thread(target=run_rpc, daemon=True)
+
+thread_icon.start()
+thread_rpc.start()
+
+thread_icon.join()
+thread_rpc.join()
