b3ec196b83d4ad3c6bf8c137f170110bc55fe61d
Rei <snowy.sunny.dazing.us@gmail.com>
2024-07-10 00:35:25 +0900

    Create tray app

---
+++ src/Television_Tray.py
@@ -0,0 +1,19 @@
+import pystray
+from PIL import Image
+
+image = Image.open("television.png")
+
+
+def action_exit(icon, _):
+    icon.stop()
+
+
+class TelevisionMenu(pystray.Menu):
+    item_exit = pystray.MenuItem("Exit", action_exit)
+
+    def __init__(self):
+        super().__init__(self.item_exit)
+
+
+icon = pystray.Icon(name="Television", icon=image, title="Television", menu=TelevisionMenu())
+icon.run()
