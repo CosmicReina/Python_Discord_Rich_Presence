470124911662d1c51ee73d48ca99be090072164c
Rei <snowy.sunny.dazing.us@gmail.com>
2024-07-12 02:23:06 +0900

    Update

--- src/television/Television.py
+++ src/television/Television.py
@@ -9,7 +9,7 @@ from PIL import Image
 from PIL.Image import StrOrBytesPath
 from pypresence import Presence
 
-import Channel
+from television.modules import Channel
 
 
 def resource_path(relative_path: StrOrBytesPath | IO[bytes]):
